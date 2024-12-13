712fda067ea578be2af8d9289d48b4215a1ff567
DUHEM Aristide <aristide.duhem@telecomnancy.eu>
2024-12-13 15:10:33 +0100

    with main

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,17 @@
 module Main (main) where
 
-import Data.Text (Text)
+import System.Environment (getArgs)
+
+import Data.Text (Text, pack)
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L -- (1)
 import Test.QuickCheck.Text (number)
 
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Aeson (Value(Bool))
 
 data Expr
   = Var String
@@ -18,7 +23,7 @@ data Expr
 data Stmt
   = Seq [Stmt]
   | Assign String Expr
-  | If Bool Stmt Stmt
+  | If Expr Stmt Stmt
   | Print Expr
   | Skip
   deriving (Show)
@@ -65,9 +70,10 @@ stmt :: Parser Stmt
 stmt = ifStmt <|> printStmt <|> skipStmt <|> assignStmt
 
 stmtSeq :: Parser Stmt
-stmtSeq = f <$> sepBy1 stmt ";"
-  -- if there's only one stmt return it without using ‘Seq’
-  where f l = if length l == 1 then head l else Seq l
+stmtSeq = do
+  stmts <- sepEndBy1 (stmt <* sc) (symbol ";")  -- Parse statements separated by semicolons and consume space after each statement
+  return $ if length stmts == 1 then head stmts else Seq stmts
+
 
 
 
@@ -80,12 +86,20 @@ exprParse = StringConst <$> stringLiteral
 ifStmt :: Parser Stmt
 ifStmt = do
   rword "if"
-  cond  <- bool
+  cond  <- exprParse
   rword "then"
-  stmt1 <- stmt
-  rword "else"
-  stmt2 <- stmt
-  return (If cond stmt1 stmt2)
+  _ <- symbol "{"
+  stmt1 <- stmtSeq
+  _ <- symbol "}"
+  elseBranch <- optional $ do
+    rword "else"
+    _ <- symbol "{"
+    stmt2 <- stmtSeq
+    _ <- symbol "}"
+    return stmt2
+  return $ case elseBranch of
+    Just stmt2 -> If cond stmt1 stmt2
+    Nothing    -> If cond stmt1 Skip
 
 assignStmt :: Parser Stmt
 assignStmt = do
@@ -103,7 +117,65 @@ printStmt = do
   expr <- exprParse
   return (Print expr)
 
+
+type Env = Map String Expr
+
+interpret :: Env -> Stmt -> IO Env
+interpret env stmt = case stmt of
+  Skip -> return env  -- Do nothing
+  
+  Assign var expr -> 
+    -- Evaluate the expression and update the environment
+    case evalExpr env expr of
+      Left err   -> error $ "Evaluation error: " ++ err
+      Right val  -> return $ Map.insert var val env
+  
+  Print expr -> 
+    -- Evaluate the expression and print its value
+    case evalExpr env expr of
+      Left err   -> error $ "Evaluation error: " ++ err
+      Right val  -> do 
+        putStrLn (show val)
+        return env
+  
+  If cond stmt1 stmt2 -> 
+    -- Evaluate the condition and choose the appropriate branch
+    case evalExpr env cond of
+      Left err    -> error $ "Condition evaluation error: " ++ err
+      Right (BoolConst True)  -> interpret env stmt1
+      Right (BoolConst False) -> interpret env stmt2
+      _ -> error "Condition did not evaluate to a boolean"
+  
+  Seq stmts -> 
+    -- Execute a sequence of statements
+    foldl interpretAndUpdate (return env) stmts
+
+interpretAndUpdate :: IO Env -> Stmt -> IO Env
+interpretAndUpdate ioEnv stmt = do
+  env <- ioEnv         -- Unwrap the current environment from IO
+  interpret env stmt    -- Interpret the current statement and return the updated environment
+
+
+evalExpr :: Env -> Expr -> Either String Expr
+evalExpr env expr = case expr of
+  Var name -> 
+    case Map.lookup name env of
+      Just val -> Right val
+      Nothing  -> Left $ "Undefined variable: " ++ name
+
+  IntConst i    -> Right (IntConst i)
+  BoolConst b   -> Right (BoolConst b)
+  StringConst s -> Right (StringConst s)
+
+
+
 main :: IO ()
 main = do
-  let x = "if false then y := 3 else print true;skip;print \"hello\""
-  parseTest stmtSeq x
+  let input = "if true then {print \"no\";} else {print \"yes\";};print \"done\""
+  case parse stmtSeq "" (Data.Text.pack input) of
+    Left err -> putStrLn $ "Parse error: " ++ errorBundlePretty err
+    Right parsedStmt -> do
+      putStrLn "Program parsed successfully!"
+      print parsedStmt
+      _ <- interpret Map.empty parsedStmt
+      return ()
